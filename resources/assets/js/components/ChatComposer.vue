55d304258868748da8b37ccb882499bf8232ebc3
Miniball #BSRU#COMSCI29#HANSA2 <winhackger@gmail.com>
2017-08-25 01:27:07 +0700

    Chat demo

---
+++ resources/assets/js/components/ChatComposer.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="chat-composer">
+        <input type="text" placeholder="พิมพ์ข้อความ..." v-model="messageText" v-on:keydown.enter="sendMessage">
+        <button class="btn btn-primary" @click="sendMessage">ส่ง</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                messageText: ''
+            }
+        },
+        methods: {
+            sendMessage () {
+                this.$emit('messagesent', {
+                    message: this.messageText,
+                    user: "Atiwat Tumsakka"
+                });
+                console.log(this.messageText);
+                this.messageText = '';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.chat-composer {
+    display: flex;
+}
+
+.chat-composer input {
+    flex: 1 auto;
+}
+
+.chat-composer button {
+    border-radius: 0;
+}
+</style>
---
+++ resources/views/chat.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Chat Room</title>
+</head>
+
+<body>
+    <div id="app">
+        <h1>Chatroom</h1>
+        <chat-log :messages="messages"></chat-log>
+        <chat-composer v-on:messagesent="addMessage"></chat-composer>
+        <!-- <chat-message></chat-message> -->
+    </div>
+    <script src="js/app.js" charset="utf-8"></script>
+</body>
+
+</html>
